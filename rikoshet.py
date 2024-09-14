71a72ffea35b4984a250c9c46a6384bb5e0d88c9
awpq14 <zrzr.9a666@gmail.com>
2024-09-14 22:24:21 +0300

    v2

--- rikoshet.py
+++ rikoshet.py
@@ -19,3 +19,65 @@ while run:
         elif e.type == MOUSEBUTTONDOWN:
             print(e.pos)
     display.update()
+import pygame
+import sys
+
+size = width, height = 800, 600
+black = 0, 0, 0
+
+
+def main():
+
+    pygame.init()
+    screen = pygame.display.set_mode(size)
+    ball_image = pygame.image.load("Без імені.png")
+    ball_rect = ball_image.get_rect()
+    ball_rect.x = 101
+    ball_rect.y = 101
+    game_over = False
+    dx = 2
+    dy = 2
+    platform_x = 325
+    platform_y = 500
+    move_right = False
+    move_left = False
+
+    while not game_over:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+        screen.fill(black)
+        pygame.draw.rect(screen, (255, 255, 255), (platform_x, platform_y, 150, 20))
+        screen.blit(ball_image, ball_rect)
+        ball_rect.x += dx
+        ball_rect.y += dy
+        #ball_rectan_x = ball_rect.x
+        #ball_rectan_y = ball_rect.y
+        #ball_rectan_x = ball_rectan_x + 150
+        #ball_rectan_y= ball_rectan_y + 150
+        if ball_rect.y < 0 or ball_rect.y > 500 and ball_rect.x > platform_x and ball_rect.x < platform_x + 150:
+            dy *= -1
+        if ball_rect.x < 0 or ball_rect.y > 500 and ball_rect.x > platform_x and ball_rect.x < platform_x + 150:
+            dx *= -1
+        if event.type == pygame.KEYDOWN:
+            if chr(event.key) == 'w':
+                move_right = True
+        elif event.type == pygame.KEYUP:
+            if chr(event.key) == 'w':
+                move_right = False
+        if move_right:
+            platform_x += 1
+        if event.type == pygame.KEYDOWN:
+            if chr(event.key) == 's':
+                move_left = True
+        elif event.type == pygame.KEYUP:
+            if chr(event.key) == 's':
+                move_left = False
+        if move_left:
+            platform_x -= 1
+        pygame.display.flip()
+        pygame.time.wait(10)
+
+
+if __name__ == '__main__':
+    main()
